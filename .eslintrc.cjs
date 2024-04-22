83dbb45ea20630d545afb23db4e31b24ac49c668
PintoGamer64 <pintogamer64@gmail.com>
2024-04-22 12:05:17 -0500

    Eslit Config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    '@electron-toolkit',
-    '@electron-toolkit/eslint-config-prettier'
+    'plugin:react/jsx-runtime'
   ]
 }
